3dff768cca05cfda7a60e7f9be8063c208cb2233
Muhammad Taufik <muhamadtauf@gmail.com>
2023-11-10 16:44:45 +0700

    package fmt

---
+++ fmt.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	firstName := "Eko"
+	lastName := "Khannedy"
+	fmt.Println("Hello '", firstName, lastName)
+	fmt.Printf("Hello '%s %s'\n", firstName, lastName)
+}
