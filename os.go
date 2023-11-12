2dc726353bfaa351ea1d6bbdb41aaa9db76944d6
Muhammad Taufik <muhamadtauf@gmail.com>
2023-11-12 21:41:43 +0700

    package os

---
+++ os.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	//menangkap argumen ketika aplikasi dijalankan
+	//contoh
+	//go run os.go eko kurniawan
+	//go run os.go "eko kurniawan"
+	args := os.Args
+	for _, arg := range args {
+		fmt.Println(arg)
+	}
+
+	hostname, err := os.Hostname()
+	if err == nil {
+		fmt.Println(hostname)
+	} else {
+		fmt.Println("Error", err.Error())
+	}
+}
